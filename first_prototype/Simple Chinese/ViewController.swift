0668fdfabf9e51e4520c0bfdd6f33934a346ecd3
Edward Lui <silui@ucsc.edu>
2017-07-06 20:44:58 -0700

    added prototype

---
+++ first_prototype/Simple Chinese/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Simple Chinese
+//
+//  Created by Luming Wang on 7/10/17.
+//  Copyright © 2017 Luming Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var name = "CCCP"
+    
+    @IBAction func BeginStudy(_ sender: Any){
+         
+    }
+    @IBAction func ToReview(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ first_prototype/Simple Chinese/ViewControllerReview.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerReview.swift
+//  Simple Chinese
+//
+//  Created by Yibo Fu on 7/13/17.
+//  Copyright © 2017 Luming Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerReview: UIViewController {
+    
+    @IBOutlet weak var label_PInYin: UILabel!
+    @IBOutlet weak var label_Character: UILabel!
+    @IBOutlet weak var label_SelectionA: UILabel!
+    @IBOutlet weak var label_SelectionB: UILabel!
+    @IBOutlet weak var label_SelectionC: UILabel!
+    @IBOutlet weak var label_SelectionD: UILabel!
+    @IBOutlet weak var button_ReturnToMain: UIButton!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
